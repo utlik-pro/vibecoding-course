ad240ceff10c46b12d84193703784d073c4f39b3
utlik-pro <mr.utlik@icloud.com>
2025-12-17 01:36:39 +0300

    feat: add student whitelist (8 emails)

--- js/config.js
+++ js/config.js
@@ -7,11 +7,16 @@ const COURSE_CONFIG = {
   // Какие модули открыты (номера 1-12)
   unlockedModules: [3, 4],
 
-  // Белый список email (эти пользователи видят ВСЕ модули)
+  // Белый список email
   whitelist: [
-    'admin@vibecoding.ru',
-    'teacher@example.com',
-    'test@test.com'
+    'tashad16a@gmail.com',           // Бондарева Наталья Александровна
+    'rudveronica@gmail.com',         // Рудко Вероника
+    'malyaruslan@gmail.com',         // Малявский Руслан
+    'ip.stasheuski@gmail.com',       // Сташевский Александр
+    'ddosattaker@gmail.com',         // Левковец Артём
+    '1952793alkhm@gmail.com',        // Хмельницкий Алексей
+    'predko1994denis@gmail.com',     // Предко Денис Андреевич
+    '1alexeikalinin1@gmail.com'      // Калинин Алексей Николаевич
   ],
 
   // Пароль админки
